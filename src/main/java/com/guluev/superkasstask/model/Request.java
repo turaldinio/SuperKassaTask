c3c51d8e350df7f0e35daaa349b1021154b118d1
Tural Guluev <tural-98@bk.ru>
2023-02-06 15:02:27 +0300

    added Request class

---
+++ src/main/java/com/guluev/superkasstask/model/Request.java
@@ -0,0 +1,15 @@
+package com.guluev.superkasstask.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Request {
+    private int id;
+    private int add;
+}
