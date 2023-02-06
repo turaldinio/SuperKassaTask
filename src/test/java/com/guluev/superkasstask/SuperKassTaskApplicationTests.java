ce0fc380fd90b1288bd1646dd51130f87f291291
Tural Guluev <tural-98@bk.ru>
2023-02-06 15:13:18 +0300

    added test

--- src/test/java/com/guluev/superkasstask/SuperKassTaskApplicationTests.java
+++ src/test/java/com/guluev/superkasstask/SuperKassTaskApplicationTests.java
@@ -1,13 +1,44 @@
 package com.guluev.superkasstask;
 
+import com.guluev.superkasstask.model.Request;
+import com.guluev.superkasstask.model.UserCurrentNumber;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 
-@SpringBootTest
-class SuperKassTaskApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class SuperKassaTaskApplicationTests {
+	@LocalServerPort
+	private String servicePort;
+
+	private String url;
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 
 	@Test
-	void contextLoads() {
+	void modify() {
+		url = String.format("http://localhost:%s/modify", servicePort);
+
+		var requestBody = new HttpEntity<>(Request.builder().id(1).add(21).build());
+
+		Assertions.assertEquals(200, testRestTemplate.postForEntity(url, requestBody, UserCurrentNumber.class).getStatusCode().value());
+
+	}
+
+	@Test
+	void exception() {
+		url = String.format("http://localhost:%s/modify", servicePort);
+
+
+		var requestBody = new HttpEntity<>(Request.builder().id(3).add(21).build());
+
+		Assertions.assertEquals(418, testRestTemplate.postForEntity(url, requestBody, String.class).getStatusCode().value());
 	}
 
 }
+
