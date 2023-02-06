af5b6e3faaba686adbb7d7bba0e217add9fe50aa
Tural Guluev <tural-98@bk.ru>
2023-02-06 15:07:16 +0300

    Added Exception controller

---
+++ src/main/java/com/guluev/superkasstask/controller/ExceptionController.java
@@ -0,0 +1,16 @@
+package com.guluev.superkasstask.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionController {
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exceptions(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.I_AM_A_TEAPOT);
+    }
+}
