34cb0de2673c6f5268b6e5c20b675f097ed745ab
Tural Guluev <tural-98@bk.ru>
2023-02-06 15:00:14 +0300

    added ExampleTable class

---
+++ src/main/java/com/guluev/superkasstask/entity/ExampleTable.java
@@ -0,0 +1,25 @@
+package com.guluev.superkasstask.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "sk_example_table")
+public class ExampleTable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    private UserCurrentNumber obj;
+}
+
