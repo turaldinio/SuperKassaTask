1919d33351660915b9f80cbab33462851b4aea66
Tural Guluev <tural-98@bk.ru>
2023-02-06 14:57:40 +0300

    initial commit

---
+++ src/main/java/com/guluev/superkasstask/SuperKassaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.guluev.superkasstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperKassaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperKassaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guluev/superkasstask/SuperKassTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guluev.superkasstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperKassTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
